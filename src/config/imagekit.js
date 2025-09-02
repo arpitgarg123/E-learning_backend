2878bbb1c21e40a93586fe8d0b889d2011db821f
arpitgarg123 <arpitgarg424@gmail.com>
2025-09-02 16:11:50 +0530

    Clean project structure: keep only one server folder

--- src/config/imagekit.js
+++
@@ -1,8 +0,0 @@
-import ImageKit from 'imagekit';
-import { env } from '../config/env.js';
-
-export const imagekit = new ImageKit({
-  publicKey: env.IMAGEKIT_PUBLIC_KEY,
-  privateKey: env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: env.IMAGEKIT_URL_ENDPOINT, // from dashboard
-});
